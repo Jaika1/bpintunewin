070e4f77d69d473f749804e4cb58ef7e8860de6c
Jack Roennfeldt <jackroennfeldt@101technology.com.au>
2024-04-16 17:17:59 +0930

    Install.ps1 tested and working on W11EMS-21H2 (AVD for AzSHCI)

--- Source/Install.ps1
+++ Source/Install.ps1
@@ -1,22 +1,28 @@
-#Mount BP ISO
-#Variables
-$logDir = "C:/BPIntuneLogs/"
-$bpImage = "BP1.12.0.998DVD.iso"
+#As you may be able to tell, Powershell is a relatively fresh thought for me. Probably room for tidy/best practices.
 
-#Mount BP image
-Write-Output "Mounting BP ISO"
-$mount = (Mount-DiskImage -ImagePath "$(Get-Location)\$($bpImage)" -PassThru | Get-Volume).DriveLetter + ":\"
+#Pre-declared Variables
+$location = $(Get-Location)
+$logDir = "C:/BPIntuneLogs/"
+
+#Find and Mount BP image
+$bpImage = Get-ChildItem -Path $location -Filter *.iso -File
+$mount = Mount-DiskImage -ImagePath "$($location)\$($bpImage)" -PassThru
+$mountDir = ($mount | Get-Volume).DriveLetter + ":\"
 
 #Install pre-reqs
-Write-Output "Installing dotnetfx3.5"
 Enable-WindowsOptionalFeature -Online -NoRestart -All -FeatureName NetFx3
-Write-Output "Installing VC Redist 32 bit"
-Start-Process -Wait -WorkingDirectory $mount -FilePath "Prerequisites\vcredist_x86.exe" -ArgumentList "/install /quiet /norestart /log `"$($logDir)vcredist_x86.txt`"" #Visual C++ Redist (32 bit)
-Write-Output "Installing VC Redist 64 bit"
-Start-Process -Wait -WorkingDirectory $mount -FilePath "Prerequisites\vcredist_x64.exe" -ArgumentList "/install /quiet /norestart /log `"$($logDir)vcredist_x64.txt`"" #Visual C++ Redist (64 bit)
-Write-Output "Installing Java Runtime Environment 6.26 32 bit"
-Start-Process -Wait -WorkingDirectory $mount -FilePath "Prerequisites\jre-6u26-windows-i586-s.exe" -ArgumentList "/s /L `"$($logDir)jre-6u26-windows-i586-s.log`"" #Java Runtime Environment 6.26 (32 bit)
+Start-Process -Wait -WorkingDirectory $mountDir -FilePath "Prerequisites\vcredist_x86.exe" -ArgumentList "/install /quiet /norestart /log `"$($logDir)vcredist_x86.txt`"" #Visual C++ Redist (32 bit)
+Start-Process -Wait -WorkingDirectory $mountDir -FilePath "Prerequisites\vcredist_x64.exe" -ArgumentList "/install /quiet /norestart /log `"$($logDir)vcredist_x64.txt`"" #Visual C++ Redist (64 bit)
+Start-Process -Wait -WorkingDirectory $mountDir -FilePath "Prerequisites\jre-6u26-windows-i586-s.exe" -ArgumentList "/s /L `"$($logDir)jre-6u26-windows-i586-s.log`"" #Java Runtime Environment 6.26 (32 bit)
 
-#Short 10s sleep for good measure
-Start-Sleep -Seconds 10
+#Copy BPconfig.ini into root of C:
+Copy-Item -Path "$($location)\BPconfig.ini" -Destination "C:\BPconfig.ini" -Force
 
+#Short 3s sleep for good measure
+Start-Sleep -Seconds 3
+
+#Begin installation of BP
+Start-Process -Wait -WorkingDirectory $mountDir -FilePath "Install\BP_SQLEx_Setup.exe" -ArgumentList "/s"
+
+#Unmount BP ISO
+$mount | Dismount-DiskImage
