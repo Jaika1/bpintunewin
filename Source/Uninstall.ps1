3774c7ef7565776c09abc3d684d2bc13b963c672
Jack Roennfeldt <jackroennfeldt@101technology.com.au>
2024-04-17 15:55:11 +0930

    Addition of a tested uninstall.ps1

--- Source/Uninstall.ps1
+++ Source/Uninstall.ps1
@@ -10,3 +10,18 @@ A script to uninstall Best Practice software. This script will not target any pr
 https://github.com/Jaika1/bpintunewin
 #>
 
+#List uninstallers tagged onto the system and look for anything containing "Bp Premier"
+$progName = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName.Contains("Bp Premier")} -ErrorAction SilentlyContinue
+#Check if anything was found and assign variable
+$installed = $null -ne $progName
+
+if ($installed -eq $True) {
+    $uninstallStr = $progName.UninstallString
+    $splitCmd = $uninstallStr -split " "
+    $uninstallCmd += $splitCmd[0] + " /S /A " + $splitCmd[1]
+    Invoke-Expression $uninstallCmd
+}
+else {
+    Write-Output "Failed to detect Best Practice installation!"
+    exit 1
+}
